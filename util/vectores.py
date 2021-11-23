d88c7fd94db96b7749e10903fefff9ada8b80c55
Pablo Franco <pabloafranco@hotmail.com>
2021-11-23 18:59:47 -0300

    4to comit

--- util/vectores.py
+++ util/vectores.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Vector:
     def __init__(self, x, y, z):
         self.x = x
@@ -22,3 +24,40 @@ class Vector:
         y = (vector.y / escalar)
         z = (vector.z / escalar)
         return(Vector(x, y, z))
+
+class Matriz:
+    _n = 0
+    _m = 0
+    _elems = None
+
+    def __init__(self, n, m, random):
+        self._n = n
+        self._m = m
+        self._elems = []
+        for i in range(n):
+            self._elems.append([])
+            for j in range(m):
+                if random == "0":
+                   self._elems[i].append(0)
+                else:
+                   self._elems[i].append(randint(0,10))
+
+    def show_matrix(self):
+        """ Imprime los valores almacenados en la matriz """
+        for i in range(self._n):
+            for j in range(self._m):
+                # Imprime de una forma elegante la matriz
+                print("| {0} ".format(self._elems[i], [j]), sep=',', end='')
+            print('|\n')                
+
+    def operacion(matriz1, matriz2, signo):
+        matrizResultado=Matriz(3,3,"0")
+        for a in range(3):
+            for b in range(3):
+                if signo=="+":
+                    matrizResultado._elems[a][b] =  matriz1._elems[a][b] + matriz2._elems[a][b] 
+                elif signo=="-":
+                    matrizResultado._elems[a][b] =  matriz1._elems[a][b] - matriz2._elems[a][b] 
+        
+        
+        return matrizResultado
